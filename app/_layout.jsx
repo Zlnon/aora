d516d8269bd96fc220390cebcb7a288cf8cf4d49
Zlnon <mtarekk666@gmail.com>
2024-06-30 20:55:18 +0300

    onbording screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -33,7 +33,7 @@ const RootLayout = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)/index" options={{headerShown:false}}/>
+      <Stack.Screen name="index" options={{headerShown:false}}/>
     </Stack>
   )
 }
